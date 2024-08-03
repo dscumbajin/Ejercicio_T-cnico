cd8d9c6b5173d96e675ada0e8746efeadfaa3bad
dscumbajin <dscumbajin@uce.edu.ec>
2024-08-02 22:46:15 -0500

    revert

--- clientes/clientes/src/main/java/com/example/clientes/exception/ClienteNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.example.clientes.exception;
-
-public class ClienteNotFoundException extends RuntimeException {
-    public ClienteNotFoundException(String mensaje) {
-        super(mensaje);
-    }
-}
--- clientes/clientes/src/main/java/com/example/clientes/exception/ClienteYaExisteException.java
+++
@@ -1,7 +0,0 @@
-package com.example.clientes.exception;
-
-public class ClienteYaExisteException extends RuntimeException {
-    public ClienteYaExisteException(String mensaje) {
-        super(mensaje);
-    }
-}
