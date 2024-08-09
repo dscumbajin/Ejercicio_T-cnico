2c078962a5f2c9c10d46a0b15d576d8aa97b441f
dscumbajin <dscumbajin@uce.edu.ec>
2024-08-09 18:52:03 -0500

    Add test movimientos

--- cuentas/src/main/java/com/example/cuentas/entity/Movimiento.java
+++ cuentas/src/main/java/com/example/cuentas/entity/Movimiento.java
@@ -18,7 +18,7 @@ public class Movimiento {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private int id;
+    private Long id;
 
     @Column(name = "fecha", nullable = false)
     private Date fecha;
